8765676f0cb81e903dce855023cf1e6fc4ec714e
Silvan <111249215+silvanirungu@users.noreply.github.com>
2023-07-24 23:16:59 +0300

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -1,4 +1,22 @@
+#include "main.h"
 
+/**
+ * print_string - Print string
+ * @list: list.
+ *
+ * Return: String length.
+ */
+
+int print_string(va_list list)
+{
+	char *p;
+	int p_len;
+
+	p = va_arg(list, char*);
+	p_len = print((p != NULL) ? p : "(null)");
+
+	return (p_len);
+}
 
 
 
